30082a212cacbb7c9088d44bbd3900c7723502f2
zerokodes <henrysam140@gmail.com>
2022-10-18 07:12:19 -0700

    Fixed bug

--- _printf.c
+++ _printf.c
@@ -8,31 +8,43 @@
 
 int _printf(const char *format, ...)
 {
-	int count, ret_value, total_length;
+	function_t identities[] = {{'c', char_type}, {'s', string_type},
+		{'i', int_type}, {'d', int_type}};
+	unsigned int count = 0, count2 = 0, idCount = 0, total_length = 0, ret_value;
+	char j = '\0';
 	va_list ap; /* ponter to argument list */
 
-	total_length = 0;
-
-	if (!format) /* if it's pointing to a null address */
-	{
+	/* if it's pointing to a null address */
+	if (!format || format == NULL || (format[count] == '%' && format[1] == '\0'))
 		return (-1);
-	}
 	va_start(ap, format); /* initialize argument list */
-	for (count = 0; format[count]; count++)
+	while (format[count])
+	{
+	for (; format[count] != '%' && format[count] != '\0'; count++)
 	{ /* to loop through string */
-		if (format[count] == '%')
-		{
+		j = format[count];
+		total_length += _putchar(j);
+	}
+	count2 = count + 1;
+		if (format[count2] == '%' && format[count])
+			_putchar('%'), total_length++, count += 2;
+			/*ret_value = format_specifier(ap, count, format);*/
+		else  if (format[count2] == '\0')
 			count++;
-			ret_value = format_specifier(ap, count, format);
-			if (ret_value == -1)
-				return (-1);
-			total_length += ret_value;
-		} /* r_value is returned by the specifier function */
 		else
-		{ /* t_length is no. of characters printed */
-			write(1, format + count, 1);
-			total_length++;
+		{
+			for (idCount = 0; identities[idCount].id && format[count]; idCount++)
+			{
+				if (identities[idCount].id == format[count2])
+				{
+					total_length += identities[idCount].f(ap);
+					count += 2;
+					break;
+				}
+			}
 		}
+		if (identities[idCount].id == '\0' && format[count])
+			_putchar(format[count++]), total_length++;
 	}
 	va_end(ap);
 	return (total_length);
